5dc07ff7399ce8e7b77a415df5ee92bb12bc0e52
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-06-02 17:37:07 +0530

    feat: add initial project setup with React, Vite, and basic styles
    Added main entry point (main.jsx) for React application.
    
    Configured Vite for development with React plugin.
    
    Created global CSS styles (index.css) for theming and layout.
    
    Included SVG assets for React and Vite logos.

--- projects/basecamp-project-management/backend/src/models/project.models.js
+++ projects/basecamp-project-management/backend/src/models/project.models.js
@@ -1,17 +1,25 @@
-import mongoose, { model, Schema } from 'mongoose';
+import mongoose, { model, Schema } from "mongoose";
 
-const projectSchema = new Schema ({
+const projectSchema = new Schema(
+  {
+    userId: {
+      type: mongoose.model.Schema.id,
+      required: true,
+      ref: "User",
+    },
     projectName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: true
-    }
-}, {
-    timestamps: true
-});
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 const projectModel = model.Project || new model("Project", projectSchema);
 
