b91e7890abe37c794e16f86b444a8ddee0e44020
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-06-11 16:42:39 +0530

    Add project task controller functions

--- projects/basecamp-project-management/backend/src/controllers/task.controllers.js
+++ projects/basecamp-project-management/backend/src/controllers/task.controllers.js
@@ -2,21 +2,88 @@ import taskModel from "../models/task.models.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import { ApiResponse } from "../utils/api-response.js";
 import { ApiError } from "../utils/api-error.js";
+import projectModel from "../models/project.models.js";
+import mongoose from "mongoose";
 
-const getTasks = asyncHandler(async (req, res) => {});
+const getTasks = asyncHandler(async (req, res) => {
+  const { projectId } = req.params;
+  if (!projectId) {
+    throw new ApiError(400, "Project id is missing");
+  }
+  const project = await projectModel.findById(projectId);
 
-const getTaskById = asyncHandler(async (req, res) => {});
+  if (!project) {
+    throw new ApiError(404, "Project not found");
+  }
 
-const createTask = asyncHandler(async (req, res) => {});
+  const tasks = await taskModel
+    .find({
+      project: new mongoose.Types.ObjectId(project._id),
+    })
+    .populate("assignedTo", "avatar username fullName");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, tasks, "Tasks fetched successfully"));
+});
+
+const getTaskById = asyncHandler(async (req, res) => {
+  const { taskId } = req.params;
+  if (!taskId) {
+    throw new ApiError(400, "Task id is missing");
+  }
+  const task = await taskModel.findById(taskId);
+  if (!task) {
+    throw new ApiError(404, "Task not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, task, "Task fetched successfully"));
+});
+
+const createTask = asyncHandler(async (req, res) => {
+  const { title, description, assignedTo, status } = req.body;
+  const { projectId } = req.params;
+
+  if (!projectId) {
+    throw new ApiError(400, "Project id is missing");
+  }
+
+  const project = await projectModel.findById(projectId);
+  if (!project) {
+    throw new ApiError(404, "Project not found");
+  }
+
+  const files = req.files || [];
+
+  const attachments = files.map((file) => {
+    return {
+      url: `${process.env.SERVER_URL}/images/${file.originalname}`,
+      mimetype: file.mimetype,
+      size: file.size,
+    };
+  });
+
+  const task = await taskModel.create({
+    title,
+    description,
+    assignedTo: assignedTo
+      ? new mongoose.Types.ObjectId(assignedTo)
+      : undefined,
+    status,
+    project: new mongoose.Types.ObjectId(project._id),
+    assignedBy: new mongoose.Types.ObjectId(req.user._id),
+    attachments,
+  });
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, attachments, "Task created successfully"));
+});
 
 const updateTask = asyncHandler(async (req, res) => {});
 
 const deleteTask = asyncHandler(async (req, res) => {});
 
-export {
-    getTasks,
-    getTaskById,
-    createTask,
-    updateTask,
-    deleteTask
-};
+export { getTasks, getTaskById, createTask, updateTask, deleteTask };
